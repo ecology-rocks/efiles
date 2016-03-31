73b9bbf610b8d82f09a485394eae585aa6337502
Sam Davis <sam@ecology.rocks>
2016-03-30 21:47:16 -0400

    pouchdb required

--- js/textEditor.js
+++ js/textEditor.js
@@ -5,52 +5,21 @@ var remote = require('remote');
 var dialog = remote.require('dialog');
 var angular =   require('angular');
 var fs = require('fs');
+var PouchDB = require('pouchdb');
+
 
 angular.module('textEditor', [])
 
     .factory('MyStory', function () {
-
     // private
         var story = "";
-
     // public
         return {
-      
             getValue: function () {
                 return story;
             },
-      
             setValue: function (val) {
                 story = val;
             }
-      
         };
-    
-    
-    })
-
-
-
-
-
-
-    .factory('EditorMode', function () {
-
-    // private
-        var editor = "";
-
-    // public
-        return {
-      
-            getValue: function () {
-                return editor;
-            },
-      
-            setValue: function (val) {
-                editor = val;
-            }
-      
-        };
-    
-    
     });
